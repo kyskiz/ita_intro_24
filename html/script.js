3f222fcb753fdae7eb57fd5565580a849bacce75
Arnoldas <arnoldas.starkauskas@swedbank.lt>
2024-03-18 11:54:28 +0200

    HTML task base

---
+++ html/script.js
@@ -0,0 +1,8 @@
+/* Task steps:
+* Add event listener to ‘.addBtn’ to add another task to list (add another li element)
+* If input field is empty show alert with message ‘Please enter task name’
+* Add ‘span’ element with ‘X’ or ‘\u00D7’ to ‘li’ elements (indicator for clickable item for removing task)
+* Hide task when clicked on close button (‘close’ class)
+* Add ‘checked’ class to LI element to mark done tasks
+
+ */
